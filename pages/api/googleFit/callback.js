d254fe24d2cc111dc32b3c41ddabba4cb7777a42
Mohsin <follow.mohsin@gmail.com>
2024-02-14 17:21:10 +0600

    update

--- pages/api/googleFit/callback.js
+++ pages/api/googleFit/callback.js
@@ -10,7 +10,8 @@ const cors = Cors({
 const oAuth2Client = new OAuth2Client(
   "1057655363182-ibsiskiieg92t41t5ofk5hvu39q6kmlq.apps.googleusercontent.com",
   "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",
-  "https://zero-fitness01.netlify.app/fit.html"
+  // "https://zero-fitness01.netlify.app/fit.html"
+  "http://localhost:3000/fit.html"
 );
 
 const handler = async (req, res) => {
--- pages/api/googleFit/index.ts
+++ pages/api/googleFit/index.ts
@@ -11,7 +11,8 @@ const cors = Cors({
 const oAuth2Client = new OAuth2Client(
   "1057655363182-ibsiskiieg92t41t5ofk5hvu39q6kmlq.apps.googleusercontent.com",
   "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",
-  "https://zero-fitness01.netlify.app/fit.html"
+  // "https://zero-fitness01.netlify.app/fit.html"
+  "http://localhost:3000/fit.html"
 );
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
